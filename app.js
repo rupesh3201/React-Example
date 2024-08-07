e61db899ae9c050d9a476de7a529ddea8f33a42c
rupesh3201 <rupesh.mandge3201@gmail.com>
2024-08-07 22:18:23 +0530

    Good Morning

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ function GreatingGM()
 {
     const date = new Date();
     const hour = date.getHours();
-    const todaydate = date.getDate();\
+    const todaydate = date.getDate();
     return
     (
         <h1>Good Morning </h1>
