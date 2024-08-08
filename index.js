54ec13a60b3a8093ca57c0c46e526dd223461d71
rupesh3201 <rupesh.mandge3201@gmail.com>
2024-08-08 23:45:08 +0530

    year

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ function SayGm()
     const year =  date.getFullYear();
 
     return
-    console.log(`Good Morning Guys"${date}:${hour}:${day}`):
+    console.log(`Good Morning Guys"${date}:${hour}:${day}:${year}`):
 
 }
