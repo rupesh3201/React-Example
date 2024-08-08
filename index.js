de2f23da9bee86c95dc934c0196502c45f543a2a
rupesh3201 <rupesh.mandge3201@gmail.com>
2024-08-08 23:41:09 +0530

    date

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ function SayGm()
     const  date =  new Date();
     const Today_date = date.getDate();
     const  hour =  date.getHours();
+    const day= date.getTime();
+    const year =  date.getFullYear();
+
     return
-    console.log("Good Morning Guy's")
+    console.log("Good Morning Guy's"${date}:${})
 
 }
